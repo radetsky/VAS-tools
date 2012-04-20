38a332369ca49dbb928c35e22a0b016f6cf9482d
Alex Radetsky <rad@netstyle.com.ua>
2012-04-20 18:06:24 +0300

    added first template

---
+++ cgi-bin/online.pl
@@ -0,0 +1,61 @@
+#!/usr/bin/env perl 
+#===============================================================================
+#
+#         FILE:  online.pl
+#
+#        USAGE:  ./online.pl 
+#
+#  DESCRIPTION:  
+#
+#      OPTIONS:  ---
+# REQUIREMENTS:  ---
+#         BUGS:  ---
+#        NOTES:  ---
+#       AUTHOR:  Alex Radetsky (Rad), <rad@rad.kiev.ua>
+#      COMPANY:  Net.Style
+#      VERSION:  1.0
+#      CREATED:  20.04.2012 18:05:59 EEST
+#     REVISION:  ---
+#===============================================================================
+
+use 5.8.0;
+use strict;
+use warnings;
+
+
+
+1;
+#===============================================================================
+
+__END__
+
+=head1 NAME
+
+online.pl
+
+=head1 SYNOPSIS
+
+online.pl
+
+=head1 DESCRIPTION
+
+FIXME
+
+=head1 EXAMPLES
+
+FIXME
+
+=head1 BUGS
+
+Unknown.
+
+=head1 TODO
+
+Empty.
+
+=head1 AUTHOR
+
+Alex Radetsky <rad@rad.kiev.ua>
+
+=cut
+
